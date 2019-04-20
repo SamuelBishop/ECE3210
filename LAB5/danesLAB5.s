fe102a66a360f44457138625acb0b8016726cad1
Samuel Bishop <sjbc35@mail.missouri.edu>
2019-04-20 17:30:47 +0000

    Teamed up with dane to improve both of our codes. Obtained working product.

--- LAB5/danesLAB5.s
+++ LAB5/danesLAB5.s
@@ -1,4 +1,4 @@
-
+////////// MACROS //////////
 .macro display meg,len
 
 	mov r7, #4				@ System number, #4 for 'write'
@@ -17,49 +17,84 @@
 	swi 0	
 .endm
 
-.macro setflag flag  
-	@ 'flag' is the address of a flag variable
+.macro setflag flag  		@ Changes a flag from 0 to 1
 	ldr r11, =\flag
 	ldr r10, =1
 	strb r10, [r11]
-
 .endm
+////////////////////////////////
 
 .data
-	flaga: .byte 0
-	flagb: .byte 0 
-	flagc: .byte 0
+	flag1: .byte 0
+	flag2: .byte 0 
+	flag3: .byte 0
 
-	Input: .space 20 
+	input: .space 20 
 
-	op1A: .space 5 
-	opr: .space 1 
-	op2A: .space 5 
+	operand1_A: .space 3 
+	operator: .space 1 
+	operand2_A: .space 3 
 	result_A: .space 10 
 	
 	operand1_H: .byte 0 
 	operand2_H: .byte 0 
 	result_H: .word 0 
 	neg_result: .byte 0 
+	
+	message1: .ascii "Enter an algebraic command line: "
+	message2: .ascii "Operand 1: " 
+	message3: .ascii "\nOperand 2: " 
+	message4: .ascii "\nOperator: " 
+	message5: .ascii "\nResult: "  
 
-	Operand1_A: .ascii "\nOperand 1: " 
-	Operand2_A: .ascii "\nOperand 2: " 
-	Operator: .ascii "\nOperator: " 
-	result: .ascii "\nResults: "  
-
-	prompt: .ascii "Enter an algebraic command line: " 
-	error1: .ascii "Error!!\nInput Format: operand1 operator operand2 \nOperand: decimal number" 
-	error2: .ascii "\nError! Inputted numbers are out of range. Must be between -128 and 127."  
-	newline: .ascii "\n" 
-	minus: .ascii "-" 
+	errorFormat: .ascii "Error!!\nInput format: Operand1 Operator Operand2\nOperand: decimal numbers\nOperators: - * / %" 
+	errorRange: .ascii "Error!! Enter operand values between -128 and 127\n"  
+	newLine: .ascii "\n" 
+	negative: .ascii "-" 
 	
 .text
 .global main
+//////////// CONVERSION FUNCTIONS //////////
+// ===== ASCII to HEX conversion ===== // 
+// r1 - address of an op#A
+// r2 - addrees of an operand#_H
+
+A_to_H:
+    push    {r4, lr} 
+    mov r8, #0
+			
+loop:                            @ a loop to go though all the numbers in each operand and convert them into hexadecimal  
+	ldrb r0, [r1], #1            @r0 = actual content in ascii     
+	cmp r0, #0    
+	BEQ finished 
+			
+    sub r0, r0, #0x30 				@subtracting 30 from ascii value to get hex value
+    mov r5, #10 
+    mov r4, r8 
+    mul r8, r4, r5
+    add r8, r8, r0 
+    b loop
+ 
+case_neg:
+	ldrb r3, [r3] 					@ r3 = flag value
+    cmp r3, #1          
+    BNE finished 
+    
+    mvn r8, r8 
+    add r8, r8, #1 
+    
+finished:
+	strb r8, [r2]				@ Store the result into operand#_H
+    pop {r4, lr}
+    bx  lr
+        
+        
+        
 // ===== HEX to ASCII conversion ===== 
 // r0 - address of a hex value variable
 // r1 - addrees of a ASCII value variable
- 
-hex_to_ascii:
+
+H_to_A:
         push    {r1, r2, r3, r5, r6, r7, r8, r9, lr}
 		ldr	r8, [r0]
 		mov		r9,	r1
@@ -94,74 +129,36 @@ store_asci:
 
         pop     {r1, r2, r3, r5, r6, r7, r8, r9, lr}
         bx      lr
-        
-// ===== ASCII to HEX conversion ===== // 
-// r1 - address of an op#A
-// r2 - addrees of an operand#_H 
-
-A_to_H:
-        push    {r4, lr}
-        @ the beginning of your Subroutine 
-        mov r8, #0
-			
-loop:                            @ a loop to go though all the numbers in each operand and convert them into hexadecimal  
-	ldrb r0, [r1], #1            @r0 = actual content in ascii     
-	cmp r0, #0    
-	BEQ exit_loop 
-			
-    sub r0, r0, #0x30 				@subtracting 30 from ascii value to get hex value
-    mov r5, #10 
-    mov r4, r8 
-    mul r8, r4, r5
-    add r8, r8, r0 
-    b loop
- 
-case_neg:
-
-	ldrb r3, [r3] 					@ r3 = flag value
-    cmp r3, #1          
-    BNE exit_loop 
-    
-    mvn r8, r8 
-    add r8, r8, #1 
-    
-exit_loop:
-	    
-	    strb r8, [r2]				@ Store the result into operand#_H
-	
-        pop {r4, lr}
-        bx  lr
 //========================================//
 
 
 main:
-		display  prompt, 33 
+		display  message1, 33 
 
-        display newline, 1 
-        input   Input, 20  
+        input input, 20  
 
-        ldr r0, =Input
-        ldr r1, =op1A 
-        ldr r2, =op2A 
-        ldr r3, =opr  
+        ldr r0, =input
+        ldr r1, =operand1_A
+        ldr r2, =operand2_A 
+        ldr r3, =operator 
 		
 num1:    @loop for 1st number  
 	
-    ldrb r4, [r0]    @loads the string into r4 
-    add r0, r0, #1    @points at the next number 
-    cmp r4, #'-'    @check to see if the first number is negative
-    BEQ    sflaga 		@if num1 is negative, set flag to 1 
+    ldrb r4, [r0]    	@ loads the string into r4 
+    add r0, r0, #1    	@ points at the next number 
+    cmp r4, #'-'    	@ check to see if the first number is negative
+    BEQ    sflag1 		@ if num1 is negative, set flag to 1 
  
-    cmp r4, #0x20    @Checks to see if there's a space 
+    cmp r4, #0x20    	@ Checks to see if there's a space 
     BEQ    loopop 
 
-    cmp r4, #0x30    @checks to see if there's a number 
+    cmp r4, #0x30    	@ checks to see if there's a number 
     BLT    error_exit 
 
-    cmp    r4, #0x39    @checks to see if there's a number 
+    cmp    r4, #0x39    @ checks to see if there's a number 
     BGT    error_exit 
     
-    strb r4, [r1]    @stores the address of 1st number (r1)
+    strb r4, [r1]    	@ stores the address of 1st number (r1)
     add r1, r1, #1 
     B num1 
 
@@ -200,7 +197,7 @@ num2:     @Loop for operand 2
     ldrb r4, [r0]    @loads the next number into register r4 
     add r0, r0, #1
     cmp r4, #0x2D    @checks to see if the number is negative
-    BEQ sflagb 
+    BEQ sflag2 
 
     cmp r4, #0xA    @checks to see if user pressed 'enter' 
     BEQ convert 
@@ -217,17 +214,17 @@ num2:     @Loop for operand 2
 
 convert:        @loop to convert ascii to hex 
 
-    ldr r1, =op1A 
+    ldr r1, =operand1_A
     ldr r2, =operand1_H
-    ldr r3, =flaga       @loads the values of flaga into r3    
+    ldr r3, =flag1       @loads the values of flag1 into r3    
     bl A_to_H
     
     mov r10, r8         
     
     
-    ldr r1, =op2A 
+    ldr r1, =operand2_A 
     ldr r2, =operand2_H
-    ldr r3, =flagb       @loads the values of flagb into r3  
+    ldr r3, =flag2       @loads the values of flag2 into r3  
     bl A_to_H
     
     mov r9, r8         
@@ -237,7 +234,7 @@ convert:        @loop to convert ascii to hex
 op: 
 
 	
-	ldr r2, =flaga 
+	ldr r2, =flag1 
     ldrb r8, [r2] 
     cmp r8,    #1    @checks if num1 is a negative number
     bne sec
@@ -248,7 +245,7 @@ op:
       
 sec:  
   
-	ldr r6, =flagb
+	ldr r6, =flag2
 	ldrb r8, [r6] 
 	cmp r8,    #1  @checks if num2 is a negative number
     
@@ -260,7 +257,7 @@ sec:
     //--------------//
 	
 contop:    
-	ldr r3, =opr         @Load address of operator  
+	ldr r3, =operator         @Load address of operator  
     ldrb r3, [r3]        @Loads value of operator into Opr
     cmp r3, #0x2B        @checks if operator +   
     BEQ add 
@@ -280,7 +277,7 @@ add:
     
     cmp r12, #0
     bgt add2
-    setflag    flagc        @result is negative set flag to 1
+    setflag    flag3        @result is negative set flag to 1
     mvn r12,r12				@make result 2's compliment
     add r12, r12, #1
     
@@ -289,7 +286,7 @@ add2:
     ldr r0, =result_H 
     strb r12, [r0] 
     ldr r1, =result_A 
-    bl hex_to_ascii 
+    bl H_to_A 
     
     b calc
 
@@ -299,7 +296,7 @@ subtract:
     
     cmp r12, #0
     bgt subtract2
-    setflag    flagc        @result is negative set flag to 1
+    setflag    flag3        @result is negative set flag to 1
     mvn r12,r12				@make result 2's compliment
     add r12, r12, #1 
     
@@ -308,7 +305,7 @@ subtract2:
     ldr r0, =result_H 
     strb r12, [r0] 
     ldr r1, =result_A 
-    bl hex_to_ascii
+    bl H_to_A
 	
 	b calc
   
@@ -318,7 +315,7 @@ multiply:
     
     cmp r12, #0
     bgt multiply2
-    setflag    flagc        @result is negative set flag to 1
+    setflag    flag3        @result is negative set flag to 1
     mvn r12,r12				@make result 2's compliment
     add r12, r12, #1
     
@@ -327,7 +324,7 @@ multiply2:
     ldr r0, =result_H 
     str r12, [r0] 
     ldr r1, =result_A 
-    bl hex_to_ascii
+    bl H_to_A
 
     b calc
   
@@ -337,7 +334,7 @@ divide:
     
     cmp r12, #0
     bgt divide2
-    setflag    flagc        @result is negative set flag to 1
+    setflag    flag3        @result is negative set flag to 1
     mvn r12,r12				@make result 2's compliment
     add r12, r12, #1
     
@@ -345,8 +342,8 @@ divide2:
         
     ldr r0, =result_H 
     strb r12, [r0] 
-    ldr r1, =result_A 
-    bl hex_to_ascii
+    ldr r1, =result_A
+    bl H_to_A
      
 calc: 
 
@@ -379,30 +376,30 @@ sizecheck:
   
 errorcheck: 
   
-    display error2, 72        @displays error if numbers are out of range
-    display newline, 1 
+    display errorRange, 49        @displays error if numbers are out of range
+    display newLine, 1 
     b exit
      
-sflaga: 
+sflag1: 
   
-    setflag    flaga        @if the 1st number is negative set flag to 1
+    setflag    flag1        @if the 1st number is negative set flag to 1
     b num1 
        
-sflagb: 
+sflag2: 
     
-    setflag    flagb        @if 2nd number is negative set flag to 1
+    setflag    flag2        @if 2nd number is negative set flag to 1
     b num2 
     
  
 error_exit: 
 
-    display error1, 74        @displays error message if input is typed wrong
+    display errorFormat, 93        @displays error message if input is typed wrong
     b exit      
 
 neg1_check: 
          
-    display Operand1_A, 12     
-    ldr r2, =flaga 
+    display message2, 11     
+    ldr r2, =flag1 
     ldrb r8, [r2] 
     cmp r8,    #1 
     BEQ neg1_print 
@@ -411,25 +408,25 @@ neg1_check:
 
 neg1_print: 
 
-        display minus, 1 
+        display negative, 1 
         b print_1 
         
 print_1: 
 
-        display op1A, 4        @prints out the first number 
-        b operator     
+        display operand1_A, 3        @prints out the first number 
+        b disOperator     
 
-operator: 
+disOperator: 
 
-        display Operator, 11 
-        display opr, 1            @prints out their operator 
+        display message4, 11 
+        display operator, 1            @prints out their operator 
         b neg2_check 
 
 neg2_check:     
 
-        ldr r6, =flagb 
+        ldr r6, =flag2 
         ldrb r8, [r6] 
-        display Operand2_A, 12 
+        display message3, 12 
         cmp r8, #1 
         BEQ neg2_print 
         
@@ -437,18 +434,18 @@ neg2_check:
 
 neg2_print: 
 
-        display minus,1 
+        display negative,1 
         b print_2 
 
 print_2:
  
-        display op2A, 4        @prints out the second number 
+        display operand2_A, 3        @prints out the second number 
    
 displays:                        @prints out the calculated result 
 
-        display result, 10 
+        display message5, 9 
         
-        ldr r10, =flagc
+        ldr r10, =flag3
         ldrb r10, [r10]
         cmp r10, #0
         BNE negativeresult 
@@ -457,13 +454,12 @@ displays:                        @prints out the calculated result
             
 negativeresult:            
 
-        display minus, 1 
+        display negative, 1 
    
 print_result: 
 
         display result_A, 10        @prints out the calculated result
-        display newline,1 
-        display newline,1 
+        display newLine,1 
 
         b exit                              
           
@@ -471,3 +467,4 @@ exit:
 	
 		@ exit system call
 		mov		r7, #1
+		swi 0
